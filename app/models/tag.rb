aba746a6b2a9ddd93f73d118a4e7c9424536e395
Adam Markowski <adam.markowski@airhelp.com>
2021-02-01 18:44:59 +0100

    Add Filter

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
   has_many :tag_messages
+
+  def filtered_messages(query)
+    tag_messages.where("text like ?", "%#{query}%")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :user_messages
+
+  def filtered_messages(query)
+    user_messages.where("text like ?", "%#{query}%")
+  end
 end
